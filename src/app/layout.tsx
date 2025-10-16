a8acae3ddbd637ed7f5d9b631e7df59ae5e7d7ae
ramjan88 <gmramjanali8888@gmail.com>
2025-10-16 23:45:12 +0600

    fixed error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Poppins } from 'next/font/google';
 import "./globals.css";
-import ResponsiveNav from "../../components/Home/Navbar/ResponsiveNav";
-import Footer from "../../components/Home/Footer/Footer";
-import LayoutWrapper from "../../components/Helper/LayoutWrapper";
+import ResponsiveNav from "../../Components/Home/Navbar/ResponsiveNav";
+import Footer from "../../Components/Home/Footer/Footer";
+import LayoutWrapper from "../../Components/Helper/LayoutWrapper";
 
 
 
